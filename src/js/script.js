e6041dbdacf444ee848cf3b20260077cb6d95128
Keyra <mrc@gmail.com>
2021-01-15 17:50:21 +0300

    add php

--- src/js/script.js
+++ src/js/script.js
@@ -93,7 +93,7 @@ $(document).ready(function(){
     validateForms('#order form');
     validateForms('#consultation form');
 
-    $('input[name=phone]').mask("+7(999) 999-99-99");
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 
     $('form').submit(function(e) {
         e.preventDefault();
