ade704ddbaf47f0eb4618fa297c5c947c1bbfe09
Keyra <mrc@gmail.com>
2021-01-15 19:56:45 +0300

    add animation

--- src/js/script.js
+++ src/js/script.js
@@ -110,4 +110,22 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //scroll
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();       
+        } else {
+            $('.pageup').fadeOut(); 
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'forprojectpulse@gmail.com';                 // Наш логин
-$mail->Password = 'forprojectPulse321321';                           // Наш пароль от ящика
+$mail->Username = 'mrc49554@gmail.com';                 // Наш логин
+$mail->Password = 'Keyraklin666';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('forprojectpulse@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('bijek71459@vy89.com');     // Add a recipient
+$mail->setFrom('mrc49554@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('forprojectpulse@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
